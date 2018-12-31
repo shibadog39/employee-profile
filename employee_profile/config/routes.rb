57a26c2375e41c7daec936fbfe0213fa085527f0
shibadog1121 <shibadog1121@gmail.com>
2018-12-31 17:31:29 +0900

    refs #1 prepare initial MVC of employee

--- employee_profile/config/routes.rb
+++ employee_profile/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'employees#index'
+  resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ employee_profile/db/migrate/20181231081843_create_employees.rb
@@ -0,0 +1,13 @@
+class CreateEmployees < ActiveRecord::Migration[5.2]
+  def change
+    create_table :employees do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
+
+      t.timestamps
+      t.index :email, unique: true
+    end
+  end
+end
