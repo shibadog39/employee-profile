473a3a90f6d39bb963d2392c8539b9154e20fed5
shibadog1121 <shibadog1121@gmail.com>
2018-12-31 23:07:45 +0900

    refs #1

--- employee_profile/app/controllers/employees_controller.rb
+++ employee_profile/app/controllers/employees_controller.rb
@@ -6,8 +6,21 @@ class EmployeesController < ApplicationController
   end
 
   def new
+    @employee = Employee.new
   end
 
   def edit
+  end
+
+  def create
+    employee = Employee.new(employee_params)
+    employee.save!
+    redirect_to employees_url, notice: "新しく「#{employee.last_name + ' ' + employee.first_name}さん」を登録しました。"
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
