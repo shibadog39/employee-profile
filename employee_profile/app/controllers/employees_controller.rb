845da0ec3acc50f0daeaa7d5960d4e877f17c38e
shibadog1121 <shibadog1121@gmail.com>
2018-12-31 23:24:44 +0900

    refs #1 set default pw

--- employee_profile/app/controllers/employees_controller.rb
+++ employee_profile/app/controllers/employees_controller.rb
@@ -14,6 +14,7 @@ class EmployeesController < ApplicationController
 
   def create
     employee = Employee.new(employee_params)
+    set_default_password employee
     employee.save!
     redirect_to employees_url, notice: "新しく「#{employee.last_name + ' ' + employee.first_name}さん」を登録しました。"
   end
@@ -22,5 +23,10 @@ class EmployeesController < ApplicationController
 
   def employee_params
     params.require(:employee).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def set_default_password employee
+    employee.password = 'password'
+    employee.password_confirmation = 'password'
   end
 end
